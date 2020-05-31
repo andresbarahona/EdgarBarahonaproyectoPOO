5ef39744bfe89a43a33767b7e4a1bc19e42297ba
Andres <user@LENOVO-PC>
2020-05-30 19:20:17 -0500

    Mi primer commit subida de tarea uno un programa en c mas conclusiones

---
+++ Edgar Barahona.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+int main ()
+{
+ cout<<"Mi nombre es Edgar Andrés Barahona Narváez " << endl;
+system("pause");
+return 0;
+}
